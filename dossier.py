7fb1c97cec1e51b2064dfa9531e056e6d5aeb64a
Minh Tri Pham <minhtri@pham.cz>
2014-10-05 02:55:44 -0400

    better NLP

--- dossier.py
+++ dossier.py
@@ -31,15 +31,18 @@ def dossierConversation(transcript):
 	transcript = json.loads(transcript)
 	information = extractInformation(transcript)
 
-	preprocessedTranscript = preprocess(transcript)
-	concepts = getTopicsFromConversation(preprocessedTranscript)
-	segmentedTranscript = generateSegmentedTranscript(transcript, information)
-
 	if "name" in information:
 		title = "Conversation with " + information["name"]
 		addCardToIndex(information)
 	else:
+		information["name"] = ""
 		title = "Conversation"
+
+	preprocessedTranscript = preprocess(transcript)
+	concepts = getTopicsFromConversation(preprocessedTranscript)
+	segmentedTranscript = generateSegmentedTranscript(transcript, information)
+
+
 
 	print information
 	print indexConversation(segmentedTranscript, information["name"], "Conversation with " + information["name"])
@@ -63,6 +66,11 @@ def indexConversation(transcript, person_id, title):
 
 #queries
 
+#all conversations
+def getConversations():
+	r = client.post('querytextindex', {'indexes': 'conversations', 'text':"*", 'print': 'all'})
+	return r.json()
+
 def getConversationAbout(topic):
 	r = client.post('querytextindex', {'indexes': 'conversations', 'text':topic, 'print': 'all'})
 	return r.json()
@@ -72,14 +80,18 @@ def getConversationWith(person_id):
 	r = client.post('querytextindex', {'indexes': 'conversations', 'text':'*', 'field_text': fieldText, 'print': 'all'})
 	return r.json()
 
+#all cards
 def getCards():
 	r = client.post('querytextindex', {'indexes': 'cards', 'text':'*', 'print': 'all'})
 	return r.json()
 
+#card of person_id
 def getCard(person_id):
 	query = person_id + ":title"
 	r = client.post('querytextindex', {'indexes': 'cards', 'text':query, 'print': 'all'})
 	return r.json()
+
+
 
 
 ### indexer
@@ -87,7 +99,7 @@ def getCard(person_id):
 def generateSegmentedTranscript(transcript, information):
 	segmentedTranscript = ""
 	meName = "Me: "
-	if "name" in information:
+	if information["name"]:
 		youName = information["name"] + ": "
 	else:
 		youName = "You: "
@@ -154,7 +166,8 @@ def extractInformation(transcript):
 		if 'you' in segment:
 			segment = segment["you"]
 			#name
-			element = listElementInString(["I'm ", "I am", "My name is ", "my name is ", "people call me ", "People call me "], segment)
+			element = listElementInString(["I'm ", "I am", "My name is ", "my name is ", 
+				"people call me ", "call me ", "Call me "], segment)
 			if element:
 				people = entityOfTypeInSegment(['Person'], segment, entities)
 				firstWord = segment.split(element)[1].split()[0]
@@ -164,7 +177,10 @@ def extractInformation(transcript):
 
 			#school
 			element = listElementInString(["I go to ", "I study at ", "I attend ", "I went to ",
-			 "I studied at ", "I attended ", "I graduated from "], segment)
+			 "I studied at ", "I attended ", "I graduated from ", "I go to the ", "I go to a ", 
+			 "I study at the ", "I study at a ", "I attend a ", "I attend the ", "I graduated from the ",
+			 "I graduated from a ", "I attended a ", "I attended the ", "I went to ", "I went to the ",
+			 "I went to a ", "I studied at the ", "I studied at a "], segment)
 			if element:
 				schools = entityOfTypeInSegment(['Organization'], segment, entities)
 				firstWord = segment.split(element)[1].split()[0]
@@ -173,7 +189,8 @@ def extractInformation(transcript):
 					information["school"] = school
 
 			#employer
-			element = listElementInString(["I work at ", "I work for "], segment)
+			element = listElementInString(["I work at ", "I work for ", "I work at a", "I work for a ",
+				"I work at the ", "I work for the ", "I work in ", "I work in the ", "I work in a"], segment)
 			if element:
 				employers = entityOfTypeInSegment(['Company', 'Organization'], segment, entities)
 				firstWord = segment.split(element)[1].split()[0]
@@ -181,9 +198,14 @@ def extractInformation(transcript):
 				if employer:
 					information["employer"] = employer
 
+				restOfSentence = segment.split(element)[1].replace("?", ".").replace("!", ".").split('.')[0]
+				jobs = entityOfTypeInSegment(['JobTitle'], restOfSentence, entities)
+				if jobs and "job" not in information:
+					information["job"] = jobs[0]
+
 			#jobtitle
-			element = listElementInString(["I work as a", "I work as an ", "I work as, " "I am a ",
-			 "I am an ", "I am ", "I'm", "I'm a ", "I'm an "], segment)
+			element = listElementInString(["I work as a", "I work as an ", "I work as, ", "I work as the " "I am a ",
+			 "I am an ", "I am the ", "I am ", "I'm", "I'm a ", "I'm an "], segment)
 			if element:
 				jobs = entityOfTypeInSegment(['JobTitle'], segment, entities)
 				firstWord = segment.split(element)[1].split()[0]
@@ -191,8 +213,14 @@ def extractInformation(transcript):
 				if job:
 					information["job"] = job
 
+				restOfSentence = segment.split(element)[1].replace("?", ".").replace("!", ".").split('.')[0]
+				employers = entityOfTypeInSegment(['Company', 'Organization'], restOfSentence, entities)
+				if employers and "employer" not in information:
+					information["employer"] = employers[0]
+
 			#hometown/homecountry
-			element = listElementInString(["I'm from ", "I am from ", "I live in "], segment)
+			element = listElementInString(["I went to school in ", "I spent my childhood in ",
+				"I'm from ", "I am from ", "I lived in", "I live in ", "I grew up in "], segment)
 			if element:
 				homes = entityOfTypeInSegment(['City', 'Country', 'StateOrCounty'], segment, entities)
 				firstWord = segment.split(element)[1].split()[0]
@@ -206,16 +234,16 @@ def extractInformation(transcript):
 				for keyword in keywords:
 					if kwsgm == keyword["text"]:
 						if keyword["sentiment"]["type"] == "positive":
-							if float(keyword["sentiment"]["score"]) < 0.6:
+							if float(keyword["sentiment"]["score"]) < 0.7:
 								continue
-							if "interests" in information:
+							if "interests" in information and kwsgm not in information["interests"]:
 								information["interests"].append(kwsgm)
 							else:
 								information["interests"] = [kwsgm]
 						elif keyword["sentiment"]["type"] == "negative":
-							if float(keyword["sentiment"]["score"]) > -0.6:
+							if float(keyword["sentiment"]["score"]) > -0.7:
 								continue
-							if "dislikes" in information:
+							if "dislikes" in information and kwsgm not in information["dislikes"]:
 								information["dislikes"].append(kwsgm)
 							else:
 								information["dislikes"] = [kwsgm]
