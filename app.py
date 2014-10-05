82264be0a3c37be3a30bdfb558138cd68e8b8d89
Sachith Gullapalli <sachith96@gmail.com>
2014-10-05 03:04:15 -0400

    endpoints added

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask
-
+import hashlib
 import dossier
+from flask import request,jsonify,redirect,Response
+import json
 
 app = Flask(__name__)
 print "Starting webapp!"
 
-@app.route('/uploadrecording')
-def index():
-		
+@app.route('/api/tester', methods = ['POST'])
+def echo():
+	return request.data
 
-
+@app.route('/api/photoupload', methods = ['POST'])
+def photoupload():
+	file = request.files['file']
+	sum=md5sum(file)
+	if file and allowed_file(sum):
+		filename = secure_filename(sum)+'.wav'
+		file.save(os.path.join('/audio/', filename))
+        return filename
+	return False		
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
