0559c88424f160ae013f058021dc4b2e4039a6cc
Minh Tri Pham <minhtri@pham.cz>
2014-10-05 09:47:16 -0400

    fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 print "Starting webapp!"
 
 
-'''
+"""
 run dossierConversation to add transcript to indexConver
 transcript must be segmented and formatted like this
 
@@ -26,38 +26,38 @@ transcript must be segmented and formatted like this
 etc
 ]
 
-'''
+"""
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return getConversations()
 
-@app.route('/', methods = ['POST'])
+@app.route("/", methods = ["POST"])
 def upload():
 	#somehow extract transcript
-	transcript = params['upload']
+	transcript = params["upload"]
 
 	card = dossier.dossierConversation(transcript)
-	return json.dumps(card['documents'])
+	return json.dumps(card["documents"])
 
-@app.route('/api/photoupload', methods = ['POST'])
+@app.route("/api/photoupload", methods = ["POST"])
 def photoupload():
-	f = request.files['file']
+	f = request.files["file"]
 	sum=hashlib.md5(f.read()).hexdigest()
 	if f:
-		filename = secure_filename(sum)+'.mp4'
+		filename = secure_filename(sum)+".mp4"
 		f.seek(0)
-		f.save(os.path.join('audio/', filename))
+		f.save(os.path.join("audio/", filename))
 		print "HELLO"
-		subprocess.call("faad -o "+os.path.join('audio/', filename[:-4])+".wav "+os.path.join('audio/', filename),shell=True)
+		subprocess.call("faad -o "+os.path.join("audio/", filename[:-4])+".wav "+os.path.join("audio/", filename),shell=True)
 		print "GO"
 		filename = filename[:-4]+".wav"
 		print "WASSUP"
-		print("sox "+os.path.join('audio/', filename)+" -c 1 "+os.path.join('audio/', "1"+filename)+" avg -l")
-		subprocess.call("sox "+os.path.join('audio/', filename)+" -c 1 "+os.path.join('audio/', "1"+filename),shell=True)
+		print("sox "+os.path.join("audio/", filename)+" -c 1 "+os.path.join("audio/", "1"+filename)+" avg -l")
+		subprocess.call("sox "+os.path.join("audio/", filename)+" -c 1 "+os.path.join("audio/", "1"+filename),shell=True)
 		filename = "1"+filename
-		signal = wavfile.read(os.path.join('audio/',filename))
+		signal = wavfile.read(os.path.join("audio/",filename))
 		data = signal[1][1000:] # delete click transient
 		print data
 		iStrt=data.size
@@ -101,8 +101,8 @@ def photoupload():
 		    if counter%4 == 1:
 		    	lastpt = i
 		    if counter%4 == 2:
-		    	wavfile.write(os.path.join('audio/',str(i)+filename),8000,speech[lastpt:i])
-		        asr_req = ASR.make_request(creds=creds, desired_asr_lang="English (US)", filename=os.path.join('audio/',i+filename))
+		    	wavfile.write(os.path.join("audio/",str(i)+filename),8000,speech[lastpt:i])
+		        asr_req = ASR.make_request(creds=creds, desired_asr_lang="English (US)", filename=os.path.join("audio/",i+filename))
 		        if asr_req.response.was_successful():
 					stuffsaid.append(asr_req.response.get_recognition_result())
 		        else:
@@ -110,8 +110,8 @@ def photoupload():
 		    if counter%4 == 3:
 		    	lastpt = i
 		    if counter%4 == 0:
-				wavfile.write(os.path.join('audio/',str(i)+filename),8000,speech[lastpt:i])
-				asr_req = ASR.make_request(creds=creds, desired_asr_lang="English (US)", filename=os.path.join('audio/',i+filename))
+				wavfile.write(os.path.join("audio/",str(i)+filename),8000,speech[lastpt:i])
+				asr_req = ASR.make_request(creds=creds, desired_asr_lang="English (US)", filename=os.path.join("audio/",i+filename))
 				if asr_req.response.was_successful():
 		 			stuffsaid.append(asr_req.response.get_recognition_result())
 				else:
@@ -120,7 +120,7 @@ def photoupload():
 		return processUnstructuredArray(stuffsaid)
 
 
-	sucks={"response":'no'}
+	sucks={"response":"no"}
 	return jsonify(sucks)		
 
 
@@ -134,36 +134,36 @@ def processUnstructuredArray(array):
 
 	response = dossier.dossierConversation(transcript)
 	print response
-	return json.dumps(response['documents'], ensure_ascii=True)
+	return json.dumps(response, ensure_ascii=True)
 
 #query methods
-@app.route('/api/getcards', methods = ['GET'])
+@app.route("/api/getcards", methods = ["GET"])
 def getCards():
 	response = dossier.getCards()
-	return json.dumps(response['documents'], ensure_ascii=True)
+	return json.dumps(response["documents"], ensure_ascii=True)
 
-@app.route('/api/getcard', methods = ['GET', 'POST'])
+@app.route("/api/getcard", methods = ["GET", "POST"])
 def getCard():
-	response = dossier.getCard(params['person'])
-	return json.dumps(response['documents'], ensure_ascii=True)
+	response = dossier.getCard(params["person"])
+	return json.dumps(response["documents"], ensure_ascii=True)
 
-@app.route('/api/getconversations', methods = ['GET'])
+@app.route("/api/getconversations", methods = ["GET"])
 def getConversations():
 	response = dossier.getConversations()
-	return json.dumps(response['documents'], ensure_ascii=True)
+	return json.dumps(response["documents"], ensure_ascii=True)
 
-@app.route('/api/getconversationswith', methods = ['GET', 'POST'])
+@app.route("/api/getconversationswith", methods = ["GET", "POST"])
 def getConversationsWith():
-	response = dossier.getConversationsWith(params['person'])
-	return json.dumps(response['documents'], ensure_ascii=True)
+	response = dossier.getConversationsWith(params["person"])
+	return json.dumps(response["documents"], ensure_ascii=True)
 
-@app.route('/api/getconversationsabout', methods = ['GET', 'POST'])
+@app.route("/api/getconversationsabout", methods = ["GET", "POST"])
 def getConversationsAbout():
-	response = dossier.getConversationsAbout(params['topic'])
-	return json.dumps(response['documents'], ensure_ascii=True)
+	response = dossier.getConversationsAbout(params["topic"])
+	return json.dumps(response["documents"], ensure_ascii=True)
 
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
 
